6f0da797b1d2e608339421f6429909c50143e87b
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-22 11:42:15 +0800

    init

---
+++ bin/ridesafe_permission_impl.dart
@@ -0,0 +1,5 @@
+import 'package:ridesafe_permission_impl/ridesafe_permission_impl.dart' as ridesafe_permission_impl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ridesafe_permission_impl.calculate()}!');
+}
---
+++ test/ridesafe_permission_impl_test.dart
@@ -0,0 +1,8 @@
+import 'package:ridesafe_permission_impl/ridesafe_permission_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
