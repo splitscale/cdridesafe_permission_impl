bc5b0ad26523deb3252cc0c3e61b3c1153940a87
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-22 12:34:37 +0800

    refactor service

--- lib/services/bluetooth_permission_service.dart
+++ lib/services/bluetooth_permission_service.dart
@@ -1,29 +1,31 @@
 import 'package:permission_handler/permission_handler.dart';
 import 'package:ridesafe_core/exceptions/service_exception.dart';
-import 'package:ridesafe_core/services/payload/service_payload.dart';
 import 'package:ridesafe_core/services/permission_service.dart';
-import 'package:ridesafe_core/services/state/service_state.dart';
 
-class BluetoothPermissionService implements PermissionService {
+class BluetoothPermissionService
+    implements
+        PermissionService<Future<Map<Permission, PermissionStatus>>,
+            Permission> {
   @override
-  Future<ServicePayload<Map<Permission, PermissionStatus>>> request() async {
+  Future<Map<Permission, PermissionStatus>> request() async {
     try {
-      final Map<Permission, PermissionStatus> payload = await [
+      return await [
         Permission.bluetooth,
         Permission.bluetoothScan,
         Permission.bluetoothAdvertise,
         Permission.bluetoothConnect,
       ].request();
-
-      return ServicePayload<Map<Permission, PermissionStatus>>(payload);
     } catch (e) {
       throw ServiceException('Permission request Failed: $e');
     }
   }
 
   @override
-  Future<ServiceState<Permission>> get state async {
-    final payload = Permission.bluetooth;
-    return ServiceState<Permission>(payload);
+  Permission get state {
+    try {
+      return Permission.bluetooth;
+    } catch (e) {
+      throw ServiceException('get permission state Failed: $e');
+    }
   }
 }
